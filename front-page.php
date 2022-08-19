02fad0cbe5baef0ccd61e60ec92a2ee32f724450
Grace Birnam <gbirnam@alphaworks.tech>
2022-08-19 09:50:11 -0700

    addad all inital template parts and scss for development start

--- front-page.php
+++ front-page.php
@@ -10,3 +10,27 @@
  */
 
 ?>
+
+<?php get_header(); ?>
+<!-- Laoding in header  -->
+
+<?php get_template_part('template-parts/banner')?> 
+<!-- Loading in Banner Section -->
+
+<?php get_template_part('template-parts/mission')?> 
+<!-- Loading in Mission Statement Section  -->
+
+<?php get_template_part('template-parts/projects')?> 
+<!-- Loading in Projects Section -->
+
+<?php get_template_part('template-parts/team')?> 
+<!-- Loading in Team Memebers Section  -->
+
+<?php get_template_part('template-parts/about')?> 
+<!-- Loading in About Section -->
+
+<?php get_template_part('template-parts/contact')?> 
+<!-- Loading in Contact Section -->
+
+<?php get_footer(); ?>
+<!-- Loading in Footer -->
