7f90b370d8994b229a9abcc6318e8d3666687d12
Grace Birnam <gbirnam@alphaworks.tech>
2022-08-23 15:40:24 -0700

    about section done no ACF yet

---
+++ template-parts/about.php
@@ -0,0 +1,19 @@
+<div class="about-section">
+    <div class="about-content">
+        <h1 class="about-header">
+            About
+        </h1>
+        <p class="about-text">
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. In vel ipsa accusamus voluptatibus, quia ipsum facilis quibusdam non iusto tempora recusandae rerum quas enim voluptatum? Cumque minima aperiam magni maiores?
+        </p>
+        <p class="about-text">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur rerum error, nulla architecto, saepe ipsa expedita quod eius, sed culpa omnis iure ea consequatur tempora nostrum quae suscipit? Non, natus.
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum facilis aperiam rem doloremque voluptatem at reprehenderit obcaecati dolorem a quod cumque aut quas atque recusandae alias enim, consequatur provident id.
+        </p>
+        <p class="about-text">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur rerum error, nulla architecto, saepe ipsa expedita quod eius, sed culpa omnis iure ea consequatur tempora nostrum quae suscipit? Non, natus.
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur rerum error, nulla architecto, saepe ipsa expedita quod eius, sed culpa omnis iure ea consequatur tempora nostrum quae suscipit? Non, natus.
+
+        </p>
+    </div>
+</div>
