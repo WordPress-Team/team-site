1b2c8aad8695a9c4ea0bef0237624c038230e76d
Grace Birnam <gbirnam@alphaworks.tech>
2022-08-23 08:48:48 -0700

    file setting

--- header.php
+++ header.php
@@ -26,23 +26,10 @@
 	<a class="skip-link screen-reader-text" href="#primary"><?php esc_html_e( 'Skip to content', 'wordpress-team-site' ); ?></a>
 
 	<header id="masthead" class="site-header">
-		<div class="site-branding">
+		<div class="site-branding">            
 			<?php
 			the_custom_logo();
-			if ( is_front_page() && is_home() ) :
-				?>
-				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-				<?php
-			else :
-				?>
-				<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
-				<?php
-			endif;
-			$wordpress_team_site_description = get_bloginfo( 'description', 'display' );
-			if ( $wordpress_team_site_description || is_customize_preview() ) :
-				?>
-				<p class="site-description"><?php echo $wordpress_team_site_description; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?></p>
-			<?php endif; ?>
+			  ?>
 		</div><!-- .site-branding -->
 
 		<nav id="site-navigation" class="main-navigation">
