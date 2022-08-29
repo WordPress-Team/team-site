d62b7de7c9e2f536fea535491c99c72eea1ac62e
Grace Birnam <gbirnam@alphaworks.tech>
2022-08-29 14:45:48 -0700

    monday 8/29/22 version

--- template-parts/team.php
+++ template-parts/team.php
@@ -4,12 +4,84 @@
 <div class="mobile-grid desktop-grid">
   <div class="circle">
     <div class="wrapper">
-    <a href="#demo-modal">Open Profile Modal</a>
+    <a href="#demo-modal1">Open Profile Modal</a>
     </div>
     <div class="member-name">
       <h2>Name</h2>
     </div>
-<div id="demo-modal" class="modal">
+<div id="demo-modal1" class="modal">
+<div class="profile-modal" id="close1">
+     
+     <div class="header">
+       <img class="profile-pic" src="" alt="">
+     </div>
+     
+     <div class="profile-info">
+       
+       <h1>Full Name</h1>
+       <p>
+         Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed a            ipsum et mi eleifend tristique a sit amet justo. Duis ornare               rhoncus arcu ultricies dignissim. Cras auctor tellus orci,              accumsan vehicula velit suscipit id. Aenean varius felis vel              interdum molestie. 
+      </p>
+        <ul class="profile-links">
+          
+          <li class="link">
+            <a class="link-icon" href="">icon</a>
+          </li>
+          
+          <li class="link">
+            <a class="link-icon" href="">icon</a>
+          </li>
+          
+          <li class="link">
+            <a class="link-icon" href="">icon</a>
+          </li>
+          
+        </ul>
+       
+         <img class="profile-logo" src="" alt="">
+       
+       
+         <ul class="profile-projects">
+           
+           <li class="project">
+              <figure>
+                <img src="" alt="">
+                <figcaption>
+                  Site Template
+                    <button class="view">
+                        <a class="viewbtn" href="" target="_blank">view</a>
+                    </button>
+                </figcaption>
+            </figure>
+           </li>
+             
+           <li class="project">
+              <figure>
+                <img src="" alt="">
+                <figcaption>
+                  Site Template
+                    <button class="view">
+                        <a class="viewbtn" href="" target="_blank">view</a>
+                    </button>
+                </figcaption>
+            </figure>
+           </li>
+           
+         </ul>
+         
+     </div>
+     <a href="#close1" class="modal__close">&times;</a>
+   </div>
+</div>
+  </div>
+  <div class="circle">
+    <div class="wrapper">
+    <a href="#demo-modal2">Open Profile Modal</a>
+    </div>
+    <div class="member-name">
+      <h2>Name</h2>
+    </div>
+<div id="demo-modal2" class="modal">
 <div class="profile-modal">
      
      <div class="header">
@@ -76,12 +148,12 @@
   </div>
   <div class="circle">
     <div class="wrapper">
-    <a href="#demo-modal">Open Profile Modal</a>
+    <a href="#demo-modal3">Open Profile Modal</a>
     </div>
     <div class="member-name">
       <h2>Name</h2>
     </div>
-<div id="demo-modal" class="modal">
+<div id="demo-modal3" class="modal">
 <div class="profile-modal">
      
      <div class="header">
@@ -148,12 +220,12 @@
   </div>
   <div class="circle">
     <div class="wrapper">
-    <a href="#demo-modal">Open Profile Modal</a>
+    <a href="#demo-modal4">Open Profile Modal</a>
     </div>
     <div class="member-name">
       <h2>Name</h2>
     </div>
-<div id="demo-modal" class="modal">
+<div id="demo-modal4" class="modal">
 <div class="profile-modal">
      
      <div class="header">
@@ -220,12 +292,12 @@
   </div>
   <div class="circle">
     <div class="wrapper">
-    <a href="#demo-modal">Open Profile Modal</a>
+    <a href="#demo-modal5">Open Profile Modal</a>
     </div>
     <div class="member-name">
       <h2>Name</h2>
     </div>
-<div id="demo-modal" class="modal">
+<div id="demo-modal5" class="modal">
 <div class="profile-modal">
      
      <div class="header">
@@ -292,12 +364,12 @@
   </div>
   <div class="circle">
     <div class="wrapper">
-    <a href="#demo-modal">Open Profile Modal</a>
+    <a href="#demo-modal6">Open Profile Modal</a>
     </div>
     <div class="member-name">
       <h2>Name</h2>
     </div>
-<div id="demo-modal" class="modal">
+<div id="demo-modal6" class="modal">
 <div class="profile-modal">
      
      <div class="header">
@@ -364,12 +436,12 @@
   </div>
   <div class="circle">
     <div class="wrapper">
-    <a href="#demo-modal">Open Profile Modal</a>
+    <a href="#demo-modal7">Open Profile Modal</a>
     </div>
     <div class="member-name">
       <h2>Name</h2>
     </div>
-<div id="demo-modal" class="modal">
+<div id="demo-modal7" class="modal">
 <div class="profile-modal">
      
      <div class="header">
@@ -436,12 +508,12 @@
   </div>
   <div class="circle">
     <div class="wrapper">
-    <a href="#demo-modal">Open Profile Modal</a>
+    <a href="#demo-modal8">Open Profile Modal</a>
     </div>
     <div class="member-name">
       <h2>Name</h2>
     </div>
-<div id="demo-modal" class="modal">
+<div id="demo-modal8" class="modal">
 <div class="profile-modal">
      
      <div class="header">
@@ -508,12 +580,12 @@
   </div>
   <div class="circle">
     <div class="wrapper">
-    <a href="#demo-modal">Open Profile Modal</a>
+    <a href="#demo-modal9">Open Profile Modal</a>
     </div>
     <div class="member-name">
       <h2>Name</h2>
     </div>
-<div id="demo-modal" class="modal">
+<div id="demo-modal9" class="modal">
 <div class="profile-modal">
      
      <div class="header">
@@ -580,12 +652,12 @@
   </div>
   <div class="circle">
     <div class="wrapper">
-    <a href="#demo-modal">Open Profile Modal</a>
+    <a href="#demo-modal10">Open Profile Modal</a>
     </div>
     <div class="member-name">
       <h2>Name</h2>
     </div>
-<div id="demo-modal" class="modal">
+<div id="demo-modal10" class="modal">
 <div class="profile-modal">
      
      <div class="header">
@@ -652,12 +724,12 @@
   </div>
   <div class="circle">
     <div class="wrapper">
-    <a href="#demo-modal">Open Profile Modal</a>
+    <a href="#demo-modal11">Open Profile Modal</a>
     </div>
     <div class="member-name">
       <h2>Name</h2>
     </div>
-<div id="demo-modal" class="modal">
+<div id="demo-modal11" class="modal">
 <div class="profile-modal">
      
      <div class="header">
@@ -724,12 +796,12 @@
   </div>
   <div class="circle">
     <div class="wrapper">
-    <a href="#demo-modal">Open Profile Modal</a>
+    <a href="#demo-modal12">Open Profile Modal</a>
     </div>
     <div class="member-name">
       <h2>Name</h2>
     </div>
-<div id="demo-modal" class="modal">
+<div id="demo-modal12" class="modal">
 <div class="profile-modal">
      
      <div class="header">
@@ -796,12 +868,12 @@
   </div>
   <div class="circle">
     <div class="wrapper">
-    <a href="#demo-modal">Open Profile Modal</a>
+    <a href="#demo-modal13">Open Profile Modal</a>
     </div>
     <div class="member-name">
       <h2>Name</h2>
     </div>
-<div id="demo-modal" class="modal">
+<div id="demo-modal13" class="modal">
 <div class="profile-modal">
      
      <div class="header">
@@ -868,15 +940,15 @@
   </div>
   <div class="circle">
     <div class="wrapper">
-    <a href="#demo-modal">Open Profile Modal</a>
+    <a href="#demo-modal14">Open Profile Modal</a>
     </div>
     <div class="member-name">
       <h2>Name</h2>
     </div>
-<div id="demo-modal" class="modal">
-<div class="profile-modal">
+<div id="demo-modal14" class="modal" id="close14">
+  <div class="profile-modal" >
      
-     <div class="header">
+    <div class="header">
        <img class="profile-pic" src="" alt="">
      </div>
      
@@ -934,79 +1006,7 @@
          </ul>
          
      </div>
-     <a href="#" class="modal__close">&times;</a>
-   </div>
-</div>
-  </div>
-  <div class="circle">
-    <div class="wrapper">
-    <a href="#demo-modal">Open Profile Modal</a>
-    </div>
-    <div class="member-name">
-      <h2>Name</h2>
-    </div>
-<div id="demo-modal" class="modal">
-<div class="profile-modal">
-     
-     <div class="header">
-       <img class="profile-pic" src="" alt="">
-     </div>
-     
-     <div class="profile-info">
-       
-       <h1>Full Name</h1>
-       <p>
-         Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed a            ipsum et mi eleifend tristique a sit amet justo. Duis ornare               rhoncus arcu ultricies dignissim. Cras auctor tellus orci,              accumsan vehicula velit suscipit id. Aenean varius felis vel              interdum molestie. 
-      </p>
-        <ul class="profile-links">
-          
-          <li class="link">
-            <a class="link-icon" href="">icon</a>
-          </li>
-          
-          <li class="link">
-            <a class="link-icon" href="">icon</a>
-          </li>
-          
-          <li class="link">
-            <a class="link-icon" href="">icon</a>
-          </li>
-          
-        </ul>
-       
-         <img class="profile-logo" src="" alt="">
-       
-       
-         <ul class="profile-projects">
-           
-           <li class="project">
-              <figure>
-                <img src="" alt="">
-                <figcaption>
-                  Site Template
-                    <button class="view">
-                        <a class="viewbtn" href="" target="_blank">view</a>
-                    </button>
-                </figcaption>
-            </figure>
-           </li>
-             
-           <li class="project">
-              <figure>
-                <img src="" alt="">
-                <figcaption>
-                  Site Template
-                    <button class="view">
-                        <a class="viewbtn" href="" target="_blank">view</a>
-                    </button>
-                </figcaption>
-            </figure>
-           </li>
-           
-         </ul>
-         
-     </div>
-     <a href="#" class="modal__close">&times;</a>
+     <a href="#close14" class="modal__close">&times;</a>
    </div>
 </div>
   </div>
