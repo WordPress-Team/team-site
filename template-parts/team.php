b3edff2bc10880fd15a0c58452b626a708b9a84a
Grace Birnam <gbirnam@alphaworks.tech>
2022-08-30 11:53:02 -0700

    finishing team section no content for team or ACF

--- template-parts/team.php
+++ template-parts/team.php
@@ -1,9 +1,10 @@
 <div class="parent">
+  <h1 class="team-title">Team</h1>
 <div class="card">
   <div class="content">
     
     <label class="modal-open modal-label" for="modal-open">
-           <p>Open Modal</p>
+           <img class="member1" src="" alt="">
      </label>
     <h1 class="member">Name</h1>
     <input type="radio" name="modal" value="open" id="modal-open" class="modal-radio" />
@@ -74,7 +75,7 @@
   <div class="content">
     
     <label class="modal-open modal-label" for="modal-open">
-           <p>Open Modal</p>
+      <img class="member2" src="" alt="">
      </label>
     <h1 class="member">Name</h1>
     <input type="radio" name="modal" value="open" id="modal-open" class="modal-radio" />
@@ -145,7 +146,7 @@
   <div class="content">
     
     <label class="modal-open modal-label" for="modal-open">
-           <p>Open Modal</p>
+      <img class="member3" src="" alt="">
      </label>
     <h1 class="member">Name</h1>
     <input type="radio" name="modal" value="open" id="modal-open" class="modal-radio" />
@@ -216,7 +217,78 @@
   <div class="content">
     
     <label class="modal-open modal-label" for="modal-open">
-           <p>Open Modal</p>
+      <img class="member4" src="" alt="">
+    </label>
+    <h1 class="member">Name</h1>
+    <input type="radio" name="modal" value="open" id="modal-open" class="modal-radio" />
+    <div class="modal">
+      <label class="modal-label overlay">
+                <input type="radio" name="modal" value="close" class="modal-radio"/>
+            </label>
+      <div class="content">
+        <div class="top">
+          <div class="profile-pic">
+            <img class="picture" src="" alt="">
+          </div>
+          <h2 class="modal-name">Name</h2>
+          <label class="modal-label close-btn">
+                    <input type="radio" name="modal" value="close" class="modal-radio"/>
+                </label>
+        </div> <!-- end of top sec -->
+        <p class="member-about">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+        <ul class="profile-links">
+          
+          <li class="link">
+            <a class="link-icon" href="">icon</a>
+          </li>
+          
+          <li class="link">
+            <a class="link-icon" href="">icon</a>
+          </li>
+          
+          <li class="link">
+            <a class="link-icon" href="">icon</a>
+          </li>
+          
+        </ul> <!-- end of links ul sec -->
+        
+        <img class="profile-logo" src="" alt="">
+        
+        <ul class="profile-projects">
+           
+           <li class="project">
+              <figure>
+                <img src="" alt="">
+                <figcaption>
+                  Site Template
+                    <button class="view">
+                        <a class="viewbtn" href="" target="_blank">view</a>
+                    </button>
+                </figcaption>
+            </figure>
+           </li>
+             
+           <li class="project">
+              <figure>
+                <img src="" alt="">
+                <figcaption>
+                  Site Template
+                    <button class="view">
+                        <a class="viewbtn" href="" target="_blank">view</a>
+                    </button>
+                </figcaption>
+            </figure>
+           </li>
+          </ul> <!-- end of projects ul sec -->
+      </div> <!-- end of content sec -->
+    </div>
+  </div> <!-- end of main content sec -->
+</div> <!-- end of card -->
+<div class="card">
+  <div class="content">
+    
+    <label class="modal-open modal-label" for="modal-open">
+      <img class="member5" src="" alt="">
      </label>
     <h1 class="member">Name</h1>
     <input type="radio" name="modal" value="open" id="modal-open" class="modal-radio" />
@@ -287,7 +359,7 @@
   <div class="content">
     
     <label class="modal-open modal-label" for="modal-open">
-           <p>Open Modal</p>
+      <img class="member6" src="" alt="">
      </label>
     <h1 class="member">Name</h1>
     <input type="radio" name="modal" value="open" id="modal-open" class="modal-radio" />
@@ -358,7 +430,7 @@
   <div class="content">
     
     <label class="modal-open modal-label" for="modal-open">
-           <p>Open Modal</p>
+      <img class="member7" src="" alt="">
      </label>
     <h1 class="member">Name</h1>
     <input type="radio" name="modal" value="open" id="modal-open" class="modal-radio" />
@@ -429,7 +501,7 @@
   <div class="content">
     
     <label class="modal-open modal-label" for="modal-open">
-           <p>Open Modal</p>
+      <img class="member8" src="" alt="">
      </label>
     <h1 class="member">Name</h1>
     <input type="radio" name="modal" value="open" id="modal-open" class="modal-radio" />
@@ -500,7 +572,7 @@
   <div class="content">
     
     <label class="modal-open modal-label" for="modal-open">
-           <p>Open Modal</p>
+      <img class="member9" src="" alt="">
      </label>
     <h1 class="member">Name</h1>
     <input type="radio" name="modal" value="open" id="modal-open" class="modal-radio" />
@@ -571,7 +643,7 @@
   <div class="content">
     
     <label class="modal-open modal-label" for="modal-open">
-           <p>Open Modal</p>
+      <img class="member10" src="" alt="">
      </label>
     <h1 class="member">Name</h1>
     <input type="radio" name="modal" value="open" id="modal-open" class="modal-radio" />
@@ -642,7 +714,7 @@
   <div class="content">
     
     <label class="modal-open modal-label" for="modal-open">
-           <p>Open Modal</p>
+      <img class="member11" src="" alt="">
      </label>
     <h1 class="member">Name</h1>
     <input type="radio" name="modal" value="open" id="modal-open" class="modal-radio" />
@@ -713,7 +785,7 @@
   <div class="content">
     
     <label class="modal-open modal-label" for="modal-open">
-           <p>Open Modal</p>
+      <img class="member12" src="" alt="">
      </label>
     <h1 class="member">Name</h1>
     <input type="radio" name="modal" value="open" id="modal-open" class="modal-radio" />
@@ -784,7 +856,7 @@
   <div class="content">
     
     <label class="modal-open modal-label" for="modal-open">
-           <p>Open Modal</p>
+      <img class="member13" src="" alt="">
      </label>
     <h1 class="member">Name</h1>
     <input type="radio" name="modal" value="open" id="modal-open" class="modal-radio" />
@@ -855,78 +927,7 @@
   <div class="content">
     
     <label class="modal-open modal-label" for="modal-open">
-           <p>Open Modal</p>
-     </label>
-    <h1 class="member">Name</h1>
-    <input type="radio" name="modal" value="open" id="modal-open" class="modal-radio" />
-    <div class="modal">
-      <label class="modal-label overlay">
-                <input type="radio" name="modal" value="close" class="modal-radio"/>
-            </label>
-      <div class="content">
-        <div class="top">
-          <div class="profile-pic">
-            <img class="picture" src="" alt="">
-          </div>
-          <h2 class="modal-name">Name</h2>
-          <label class="modal-label close-btn">
-                    <input type="radio" name="modal" value="close" class="modal-radio"/>
-                </label>
-        </div> <!-- end of top sec -->
-        <p class="member-about">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-        <ul class="profile-links">
-          
-          <li class="link">
-            <a class="link-icon" href="">icon</a>
-          </li>
-          
-          <li class="link">
-            <a class="link-icon" href="">icon</a>
-          </li>
-          
-          <li class="link">
-            <a class="link-icon" href="">icon</a>
-          </li>
-          
-        </ul> <!-- end of links ul sec -->
-        
-        <img class="profile-logo" src="" alt="">
-        
-        <ul class="profile-projects">
-           
-           <li class="project">
-              <figure>
-                <img src="" alt="">
-                <figcaption>
-                  Site Template
-                    <button class="view">
-                        <a class="viewbtn" href="" target="_blank">view</a>
-                    </button>
-                </figcaption>
-            </figure>
-           </li>
-             
-           <li class="project">
-              <figure>
-                <img src="" alt="">
-                <figcaption>
-                  Site Template
-                    <button class="view">
-                        <a class="viewbtn" href="" target="_blank">view</a>
-                    </button>
-                </figcaption>
-            </figure>
-           </li>
-          </ul> <!-- end of projects ul sec -->
-      </div> <!-- end of content sec -->
-    </div>
-  </div> <!-- end of main content sec -->
-</div> <!-- end of card -->
-<div class="card">
-  <div class="content">
-    
-    <label class="modal-open modal-label" for="modal-open">
-           <p>Open Modal</p>
+      <img class="member14" src="" alt="">
      </label>
     <h1 class="member">Name</h1>
     <input type="radio" name="modal" value="open" id="modal-open" class="modal-radio" />
