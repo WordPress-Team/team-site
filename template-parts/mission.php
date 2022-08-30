6c9d06812c645a0997afdf695fba17d81b5b6888
KyleWashington73 <91575541+KyleWashington73@users.noreply.github.com>
2022-08-29 19:13:26 -0700

    need to complete slider

--- template-parts/mission.php
+++ template-parts/mission.php
@@ -13,6 +13,89 @@
    </section>
 
    <section class="section-2">
+    <div class="slide-container">
+      <span class="slider-span" id="slider-span1"></span>
+      <span class="slider-span" id="slider-span2"></span>
+      <span class="slider-span" id="slider-span3"></span>
+      <span class="slider-span" id="slider-span4"></span>
+      <span class="slider-span" id="slider-span5"></span>
+      <span class="slider-span" id="slider-span6"></span>
+      <span class="slider-span" id="slider-span7"></span>
+      <span class="slider-span" id="slider-span8"></span>
+      <span class="slider-span" id="slider-span9"></span>
+      <span class="slider-span" id="slider-span10"></span>
+      <span class="slider-span" id="slider-span11"></span>
+      <span class="slider-span" id="slider-span12"></span>
+      <span class="slider-span" id="slider-span13"></span>
+      <span class="slider-span" id="slider-span14"></span>
+      <span class="slider-span" id="slider-span15"></span>
+
+      <div class="image-slider">
+        <div class="slides-div" id="slide-1">
+            <img src="http://impact-zone.local/wp-content/uploads/2022/08/image5-scaled.jpg" alt="" class="img" id="img1">
+            <a href="#slider-span1" class="button" id="button-1"></a>
+        </div>
+        <div class="slides-div" id="slide-2">
+            <img src="http://impact-zone.local/wp-content/uploads/2022/08/image6-scaled.jpg" alt="" class="img" id="img2">
+            <a href="#slider-span1" class="button" id="button-2"></a>
+        </div>
+        <div class="slides-div" id="slide-3 ">
+            <img src="http://impact-zone.local/wp-content/uploads/2022/08/image7-scaled.jpg" alt="" class="img" id="img3">
+            <a href="#slider-span1" class="button" id="button-3"></a>
+        </div>
+        <div class="slides-div" id="slide-4">
+            <img src="hhttp://impact-zone.local/wp-content/uploads/2022/08/image8-scaled.jpg" alt="" class="img" id="img4">
+            <a href="#slider-span1" class="button" id="button-4"></a>
+        </div>
+        <div class="slides-div" id="slide-5">
+            <img src="http://impact-zone.local/wp-content/uploads/2022/08/image9-scaled.jpg" alt="" class="img" id="img5">
+            <a href="#slider-span1" class="button" id="button-5"></a>
+        </div>
+        <div class="slides-div" id="slide-6">
+            <img src="http://impact-zone.local/wp-content/uploads/2022/08/image10-scaled.jpg" alt="" class="img" id="img6">
+            <a href="#slider-span1" class="button" id="button-6"></a>
+        </div>
+        <div class="slides-div" id="slide-7">
+            <img src=http://impact-zone.local/wp-content/uploads/2022/08/image11-scaled.jpg alt=""class="img" id="img7">
+            <a href="#slider-span1" class="button" id="button-7"></a>
+        </div>
+        <div class="slides-div" id="slide-8">
+            <img src="http://impact-zone.local/wp-content/uploads/2022/08/image12-scaled.jpg" alt="" class="img" id="img8">
+            <a href="#slider-span1" class="button" id="button-8"></a>
+        </div>
+        <div class="slides-div" id="slide-9">
+            <img src="http://impact-zone.local/wp-content/uploads/2022/08/image13-scaled.jpg" alt="" class="img" id="img9">
+            <a href="#slider-span1" class="button" id="button-9"></a>
+        </div>
+        <div class="slides-div" id="slide-10">
+            <img src="http://impact-zone.local/wp-content/uploads/2022/08/image14-scaled.jpg" alt="" class="img" id="img10">
+            <a href="#slider-span1" class="button" id="button-10"></a>
+        </div>
+        <div class="slides-div" id="slide-11">
+            <img src="hhttp://impact-zone.local/wp-content/uploads/2022/08/image16-scaled.jpg" alt="" class="img" id="img11">
+            <a href="#slider-span1" class="button" id="button-11"></a>
+        </div>
+        <div class="slides-div" id="slide-12">
+            <img src="http://impact-zone.local/wp-content/uploads/2022/08/image3-1-scaled.jpg" alt="" class="img" id="img12">
+            <a href="#slider-span1" class="button" id="button-12"></a>
+        </div>
+        <div class="slides-div" id="slide-13">
+            <img src="http://impact-zone.local/wp-content/uploads/2022/08/image20-scaled.jpg" alt="" class="img" id="img13">
+            <a href="#slider-span1" class="button" id="button-13"></a>
+        </div>
+        <div class="slides-div" id="slide-14">
+            <img src="http://impact-zone.local/wp-content/uploads/2022/08/image21-scaled.jpg" alt="" class="img" id="img14">
+            <a href="#slider-span1" class="button" id="button-14"></a>
+        </div>
+        <div class="slides-div" id="slide-15">
+            <img src="http://impact-zone.local/wp-content/uploads/2022/08/image4-scaled.jpg" alt="" class="img" id="img15">
+            <a href="#slider-span1" class="button" id="button-15"></a>
+        </div>
+      </div>
+     
+
+    </div>
+    
    
    </section>
 
