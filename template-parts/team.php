bf889f29dfbf57c44cb9319bbec215cb7544fb76
Grace Birnam <gbirnam@alphaworks.tech>
2022-08-30 16:59:19 -0700

    almost done logo modal no ACF

--- template-parts/team.php
+++ template-parts/team.php
@@ -3,11 +3,11 @@
 <div class="card">
   <div class="content">
     
-    <label class="modal-open modal-label" for="modal-open">
+    <label class="modal-open modal-label" for="modal-open-one">
            <img class="member1" src="" alt="">
      </label>
     <h1 class="member">Name</h1>
-    <input type="radio" name="modal" value="open" id="modal-open" class="modal-radio" />
+    <input type="radio" name="modal" value="open" id="modal-open-one" class="modal-radio" />
     <div class="modal">
       <label class="modal-label overlay">
                 <input type="radio" name="modal" value="close" class="modal-radio"/>
@@ -74,11 +74,11 @@
 <div class="card">
   <div class="content">
     
-    <label class="modal-open modal-label" for="modal-open">
+    <label class="modal-open modal-label" for="modal-open-two">
       <img class="member2" src="" alt="">
      </label>
     <h1 class="member">Name</h1>
-    <input type="radio" name="modal" value="open" id="modal-open" class="modal-radio" />
+    <input type="radio" name="modal" value="open" id="modal-open-two" class="modal-radio" />
     <div class="modal">
       <label class="modal-label overlay">
                 <input type="radio" name="modal" value="close" class="modal-radio"/>
@@ -145,11 +145,11 @@
 <div class="card">
   <div class="content">
     
-    <label class="modal-open modal-label" for="modal-open">
+    <label class="modal-open modal-label" for="modal-open-three">
       <img class="member3" src="" alt="">
      </label>
     <h1 class="member">Name</h1>
-    <input type="radio" name="modal" value="open" id="modal-open" class="modal-radio" />
+    <input type="radio" name="modal" value="open" id="modal-open-three" class="modal-radio" />
     <div class="modal">
       <label class="modal-label overlay">
                 <input type="radio" name="modal" value="close" class="modal-radio"/>
@@ -216,11 +216,11 @@
 <div class="card">
   <div class="content">
     
-    <label class="modal-open modal-label" for="modal-open">
+    <label class="modal-open modal-label" for="modal-open-four">
       <img class="member4" src="" alt="">
     </label>
     <h1 class="member">Name</h1>
-    <input type="radio" name="modal" value="open" id="modal-open" class="modal-radio" />
+    <input type="radio" name="modal" value="open" id="modal-open-four" class="modal-radio" />
     <div class="modal">
       <label class="modal-label overlay">
                 <input type="radio" name="modal" value="close" class="modal-radio"/>
@@ -287,11 +287,11 @@
 <div class="card">
   <div class="content">
     
-    <label class="modal-open modal-label" for="modal-open">
+    <label class="modal-open modal-label" for="modal-open-five">
       <img class="member5" src="" alt="">
      </label>
     <h1 class="member">Name</h1>
-    <input type="radio" name="modal" value="open" id="modal-open" class="modal-radio" />
+    <input type="radio" name="modal" value="open" id="modal-open-five" class="modal-radio" />
     <div class="modal">
       <label class="modal-label overlay">
                 <input type="radio" name="modal" value="close" class="modal-radio"/>
@@ -358,11 +358,11 @@
 <div class="card">
   <div class="content">
     
-    <label class="modal-open modal-label" for="modal-open">
+    <label class="modal-open modal-label" for="modal-open-six">
       <img class="member6" src="" alt="">
      </label>
     <h1 class="member">Name</h1>
-    <input type="radio" name="modal" value="open" id="modal-open" class="modal-radio" />
+    <input type="radio" name="modal" value="open" id="modal-open-six" class="modal-radio" />
     <div class="modal">
       <label class="modal-label overlay">
                 <input type="radio" name="modal" value="close" class="modal-radio"/>
@@ -429,11 +429,11 @@
 <div class="card">
   <div class="content">
     
-    <label class="modal-open modal-label" for="modal-open">
+    <label class="modal-open modal-label" for="modal-open-seven">
       <img class="member7" src="" alt="">
      </label>
     <h1 class="member">Name</h1>
-    <input type="radio" name="modal" value="open" id="modal-open" class="modal-radio" />
+    <input type="radio" name="modal" value="open" id="modal-open-seven" class="modal-radio" />
     <div class="modal">
       <label class="modal-label overlay">
                 <input type="radio" name="modal" value="close" class="modal-radio"/>
@@ -500,11 +500,11 @@
 <div class="card">
   <div class="content">
     
-    <label class="modal-open modal-label" for="modal-open">
+    <label class="modal-open modal-label" for="modal-open-eight">
       <img class="member8" src="" alt="">
      </label>
     <h1 class="member">Name</h1>
-    <input type="radio" name="modal" value="open" id="modal-open" class="modal-radio" />
+    <input type="radio" name="modal" value="open" id="modal-open-eight" class="modal-radio" />
     <div class="modal">
       <label class="modal-label overlay">
                 <input type="radio" name="modal" value="close" class="modal-radio"/>
@@ -571,11 +571,11 @@
 <div class="card">
   <div class="content">
     
-    <label class="modal-open modal-label" for="modal-open">
+    <label class="modal-open modal-label" for="modal-open-nine">
       <img class="member9" src="" alt="">
      </label>
     <h1 class="member">Name</h1>
-    <input type="radio" name="modal" value="open" id="modal-open" class="modal-radio" />
+    <input type="radio" name="modal" value="open" id="modal-open-nine" class="modal-radio" />
     <div class="modal">
       <label class="modal-label overlay">
                 <input type="radio" name="modal" value="close" class="modal-radio"/>
@@ -642,11 +642,11 @@
 <div class="card">
   <div class="content">
     
-    <label class="modal-open modal-label" for="modal-open">
+    <label class="modal-open modal-label" for="modal-open-ten">
       <img class="member10" src="" alt="">
      </label>
     <h1 class="member">Name</h1>
-    <input type="radio" name="modal" value="open" id="modal-open" class="modal-radio" />
+    <input type="radio" name="modal" value="open" id="modal-open-ten" class="modal-radio" />
     <div class="modal">
       <label class="modal-label overlay">
                 <input type="radio" name="modal" value="close" class="modal-radio"/>
@@ -713,11 +713,11 @@
 <div class="card">
   <div class="content">
     
-    <label class="modal-open modal-label" for="modal-open">
+    <label class="modal-open modal-label" for="modal-open-eleven">
       <img class="member11" src="" alt="">
      </label>
     <h1 class="member">Name</h1>
-    <input type="radio" name="modal" value="open" id="modal-open" class="modal-radio" />
+    <input type="radio" name="modal" value="open" id="modal-open-eleven" class="modal-radio" />
     <div class="modal">
       <label class="modal-label overlay">
                 <input type="radio" name="modal" value="close" class="modal-radio"/>
@@ -784,11 +784,11 @@
 <div class="card">
   <div class="content">
     
-    <label class="modal-open modal-label" for="modal-open">
+    <label class="modal-open modal-label" for="modal-open-twelve">
       <img class="member12" src="" alt="">
      </label>
     <h1 class="member">Name</h1>
-    <input type="radio" name="modal" value="open" id="modal-open" class="modal-radio" />
+    <input type="radio" name="modal" value="open" id="modal-open-twelve" class="modal-radio" />
     <div class="modal">
       <label class="modal-label overlay">
                 <input type="radio" name="modal" value="close" class="modal-radio"/>
@@ -855,11 +855,11 @@
 <div class="card">
   <div class="content">
     
-    <label class="modal-open modal-label" for="modal-open">
+    <label class="modal-open modal-label" for="modal-open-thirteen">
       <img class="member13" src="" alt="">
      </label>
     <h1 class="member">Name</h1>
-    <input type="radio" name="modal" value="open" id="modal-open" class="modal-radio" />
+    <input type="radio" name="modal" value="open" id="modal-open-thirteen" class="modal-radio" />
     <div class="modal">
       <label class="modal-label overlay">
                 <input type="radio" name="modal" value="close" class="modal-radio"/>
@@ -926,11 +926,11 @@
 <div class="card">
   <div class="content">
     
-    <label class="modal-open modal-label" for="modal-open">
+    <label class="modal-open modal-label" for="modal-open-logo">
       <img class="member14" src="" alt="">
      </label>
     <h1 class="member">Name</h1>
-    <input type="radio" name="modal" value="open" id="modal-open" class="modal-radio" />
+    <input type="radio" name="modal" value="open" id="modal-open-logo" class="modal-radio" />
     <div class="modal">
       <label class="modal-label overlay">
                 <input type="radio" name="modal" value="close" class="modal-radio"/>
@@ -938,60 +938,42 @@
       <div class="content">
         <div class="top">
           <div class="profile-pic">
-            <img class="picture" src="" alt="">
+            <div class="picture-border">
+            <img class="picture" src="http://wordpress-team-site.local/wp-content/uploads/2022/08/Impact-Zone-Logo-2.png" alt="">
+            </div>
           </div>
-          <h2 class="modal-name">Name</h2>
+          <h2 class="modal-name-logo">Impact Zone</h2>
           <label class="modal-label close-btn">
                     <input type="radio" name="modal" value="close" class="modal-radio"/>
                 </label>
         </div> <!-- end of top sec -->
-        <p class="member-about">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-        <ul class="profile-links">
-          
-          <li class="link">
-            <a class="link-icon" href="">icon</a>
+        <p class="member-about-logo">How did our logo come about?</p>
+        <ul class="logo-explained">
+          <li class="map">
+            <img class="map-ref"src="http://wordpress-team-site.local/wp-content/uploads/2022/08/Team-Logo-Sample-1-e1661895699813.png" alt="">
+            <h4 class="logo-about">
+              We as a team are located all throught CA. Our team member Danu Vong came up with the idea to utilize the freeway maps that connect us all.
+            </h4>
           </li>
           
-          <li class="link">
-            <a class="link-icon" href="">icon</a>
+          <li class="triangle">
+            <img class="triangle-ref"src="http://wordpress-team-site.local/wp-content/uploads/2022/08/Team-Logo-Sample-2.png" alt="">
+            <h4 class="logo-about">
+              We then utilized a triangle to symbolize unity between us all.
+            </h4>
           </li>
           
-          <li class="link">
-            <a class="link-icon" href="">icon</a>
+          <li class="zone">
+            <img class="zone-ref"src="http://wordpress-team-site.local/wp-content/uploads/2022/08/image-2-e1661895923218.png" alt="">
+            <h4 class="logo-about">
+       
+            </h4>           
           </li>
-          
-        </ul> <!-- end of links ul sec -->
+        </ul>
         
-        <img class="profile-logo" src="" alt="">
-        
-        <ul class="profile-projects">
-           
-           <li class="project">
-              <figure>
-                <img src="" alt="">
-                <figcaption>
-                  Site Template
-                    <button class="view">
-                        <a class="viewbtn" href="" target="_blank">view</a>
-                    </button>
-                </figcaption>
-            </figure>
-           </li>
-             
-           <li class="project">
-              <figure>
-                <img src="" alt="">
-                <figcaption>
-                  Site Template
-                    <button class="view">
-                        <a class="viewbtn" href="" target="_blank">view</a>
-                    </button>
-                </figcaption>
-            </figure>
-           </li>
-          </ul> <!-- end of projects ul sec -->
+     
       </div> <!-- end of content sec -->
     </div>
   </div> <!-- end of main content sec -->
-</div> <!-- end of card -->
+ </div> <!-- end of card -->
 </div> <!-- end of parent -->
