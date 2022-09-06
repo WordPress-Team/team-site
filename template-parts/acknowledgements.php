81dfeba5f7c970d6aa3824c5c97a697f0a5ec40e
KyleWashington73 <91575541+KyleWashington73@users.noreply.github.com>
2022-09-06 16:08:23 -0700

    about ready to begin styling sections

--- template-parts/acknowledgements.php
+++ template-parts/acknowledgements.php
@@ -1,33 +1,24 @@
 <section class="first-section">
-    <h1>ACKNOWLEDGEMENTS</h1>
-    
-<div class="content-flow">
+    <h1 class="header">ACKNOWLEDGEMENTS</h1>
+   
+  <div class="container">
       <div class="even-columns">
         <div class="col">
           <img src="http://impact-zone.local/wp-content/uploads/2022/09/icon.jpg" alt="">
-          <h2>Engage</h2>
-          <p>with Youth to Promote Change</p>
+          <p class="paragraph">Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente sint ducimus perferendis harum maxime fugiat error nostrum quos accusamus expedita, voluptatibus, nisi provident cumque numquam tempora, explicabo illo atque eaque.</p>
         </div>
         <div class="col">
           <img src="http://impact-zone.local/wp-content/uploads/2022/09/icon.jpg" alt="">
-          <h2>Implement</h2>
-          <p>Evidence-Based & Innovative Programs</p>
+          <p class="paragraph">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sunt inventore, obcaecati tenetur perferendis fugiat soluta totam officiis magnam, non voluptate incidunt illo neque at blanditiis.</p>
         </div>
         <div class="col">
           <img src="http://impact-zone.local/wp-content/uploads/2022/09/icon.jpg" alt="">
-          <h2>Locate</h2>
-          <p>Program Funding in Your Community</p>
+          <p class="paragraph">Lorem ipsum dolor sit amet consectetur adipisicing elit. At placeat possimus magni illo excepturi. Maiores explicabo dolorem optio laborum quo voluptates libero molestias recusandae distinctio? Provident quo quidem totam officiis!</p>
         </div>
         <div class="col">
           <img src="http://impact-zone.local/wp-content/uploads/2022/09/icon.jpg" alt="">
-          <h2>Locate</h2>
-          <p>Program Funding in Your Community</p>
+          <p class="paragraph">Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente tenetur rem perspiciatis saepe repellat, itaque nam dicta sit natus dolor quis? Iure earum porro doloremque reiciendis dolorum.</p>
         </div>
-        
-      </div>   
-
-
-
-
-
+      </div>
+  </div>   
 </section>
