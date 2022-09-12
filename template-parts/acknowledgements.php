c8c0d01955494d50778c9b3f18ab63ee8b4a7b42
KyleWashington73 <91575541+KyleWashington73@users.noreply.github.com>
2022-09-12 16:04:52 -0700

    acknowledgements section is almost complete

--- template-parts/acknowledgements.php
+++ template-parts/acknowledgements.php
@@ -1,25 +1,38 @@
 <section class="first-section">
    
    
-  <div class="container">
+  <div class="container-1">
     <h1 class="header">ACKNOWLEDGEMENTS</h1>
-      <div class="even-columns">
-        <div class="col">
+      <div class="even-columns-1">
+        <div class="col-1">
           <img src="http://impact-zone.local/wp-content/uploads/2022/09/icon.jpg" class="image" alt="">
           <p class="paragraph">Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente sint ducimus perferendis harum maxime fugiat error nostrum quos accusamus expedita, voluptatibus, nisi provident cumque numquam tempora, explicabo</p>
         </div>
-        <div class="col">
+        <div class="col-2">
           <img src="http://impact-zone.local/wp-content/uploads/2022/09/icon.jpg" class="image" alt="">
           <p class="paragraph">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sint in commodi incidunt aliquam recusandae ex, omnis libero quam nulla possimus. Hic eaque animi recusandae ut. Modi quasi unde at distinctio libero earum sequi necessitatibus repellendus!</p>
         </div>
-        <div class="col">
+      <!-- </div>
+  </div>    -->
+
+  <!-- <div class="container-2"> -->
+      <div class="even-columns-2">
+        <div class="col-3">
           <img src="http://impact-zone.local/wp-content/uploads/2022/09/icon.jpg"  class="image" alt="">
           <p class="paragraph">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Distinctio ratione asperiores accusamus, at nam nostrum architecto odio aperiam natus maiores ad ducimus, tempore nulla perferendis quibusdam consectetur, nesciunt exercitationem. </p>
         </div>
-        <div class="col">
+        <div class="col-4">
           <img src="http://impact-zone.local/wp-content/uploads/2022/09/icon.jpg" class="image" alt="">
           <p class="paragraph">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Veritatis voluptatibus non, dicta harum tempora officiis laborum ipsam illum voluptatum magni libero nesciunt reprehenderit, sit dolores voluptatem esse mollitia nemo, est illo temporibus itaque!</p>
         </div>
       </div>
   </div>   
 </section>
+
+
+
+
+
+
+
+
