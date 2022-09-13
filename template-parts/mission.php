16492343493101738326a48d1abd69a34aa388bc
KyleWashington73 <91575541+KyleWashington73@users.noreply.github.com>
2022-09-12 17:57:21 -0700

    need to install new slider

--- template-parts/mission.php
+++ template-parts/mission.php
@@ -4,7 +4,7 @@
    <section class="section-1">
     <h1 class="mission-header">Our Mission</h1>
     <div class="our-mission-1-container">
-        <p class="our-mission-1-container">The mission of Impact Zone is to asssist in our clients 
+        <p class="our-mission-1-container">The mission of Impact Zone is to asssist our clients 
         in reflecting their dreams, visions, and ideas onto a webpage; <br> connecting 
         with and affecting the greater community through deliberate and meaningful action. 
         We do this by <br> offering services in web design and development using WordPress to 
