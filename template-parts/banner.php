6aad9421a9836ddc6d94282b6421149a2f1ceabd
DanuVong <dvong@alphaworks.tech>
2022-09-16 14:32:55 -0700

    made changes to banner buttons

--- template-parts/banner.php
+++ template-parts/banner.php
@@ -1,4 +1,4 @@
-<section>
+<section id="hero-link">
 <h1 class="banner-1">Imp<span class="logo-a"><img class="logo-letter" src="http://impact-zone.local/wp-content/uploads/2022/08/Impact-Zone-Logo-2.png"></span>ct Zone</h1>
 
 <div class="banner-container">
@@ -6,7 +6,7 @@
       <p class="paragraph">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Pharetra et ultrices neque ornare aenean.</p>
 
       <div class="both-buttons">
-        <button class="button-1" type="button">IMPACT</button>
+        <button class="button-1" type="button">Learn More</button>
         <button class="button-2" type="button">ZONE</button>
     </div>
 </div>
