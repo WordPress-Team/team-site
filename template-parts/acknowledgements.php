21c0c0979e3ab6478e1d527c66a8db094eb29fb5
KyleWashington73 <91575541+KyleWashington73@users.noreply.github.com>
2022-09-21 00:33:01 -0700

    need to add modals

--- template-parts/acknowledgements.php
+++ template-parts/acknowledgements.php
@@ -5,25 +5,25 @@
       <div class="even-columns-1">
         <div class="col-1">
           <img src="http://impact-zone.local/wp-content/uploads/2022/09/icon.jpg" class="image" alt="">
-          <h3 class="title">Aryca Gordon (She/Her·air-wreckkah), Persevere - Vice President, Pre-Apprenticeships</h3>
+          <h3 class="title">Aryca Gordon (She/Her), Persevere - Vice President, Pre-Apprenticeships</h3>
           <p class="paragraph p1">'Impact Zone' would like to pay regard to Ms. Gordon for her undying dedication to and continued support for her students/apprentices here at Alphaworks. Thank you for being the heart and soul of this apprenticeship; you are genuinely appreciated!</p>
         </div>
         <div class="col-2">
           <img src="http://impact-zone.local/wp-content/uploads/2022/09/icon.jpg" class="image" alt="">
           <h3 class="title">Stephanie Moreno (She/Her/Ella), VP of Apprenticeship</h3>
-          <p class="paragraph p2">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sint in commodi incidunt aliquam recusandae ex, omnis libero quam nulla possimus. Hic eaque animi recusandae ut. Modi quasi unde at distinctio libero earum sequi necessitatibus repellendus!</p>
+          <p class="paragraph p2">'Impact Zone' would like to give credit where credit is due and honor Ms. Moreno with being a phenomenal champion for the students/apprentices here at Alphaworks. We thank you for your adherance to the continued quality of our apprenticeship program! </p>
         </div>
       </div>
       <div class="even-columns-2">
         <div class="col-3">
           <img src="http://impact-zone.local/wp-content/uploads/2022/09/icon.jpg"  class="image" alt="">
           <h3 class="title">Gigi Pruett (She/Her/Ella), Apprentice Lead Coordinator</h3>
-          <p class="paragraph p3">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Distinctio ratione asperiores accusamus, at nam nostrum architecto odio aperiam natus maiores ad ducimus, tempore nulla perferendis quibusdam consectetur, nesciunt exercitationem. </p>
+          <p class="paragraph p3">'Impact Zone' would like to recognize Ms. Pruett for her constant encouragement and wholehearted commitment to the students/apprentices here at Alphaworks. Thank you for taking the time to let us know that we are loved!</p>
         </div>
         <div class="col-4">
           <img src="http://impact-zone.local/wp-content/uploads/2022/09/icon.jpg" class="image" alt="">
           <h3 class="title">Sarah 'Raz' Raspberry (She/Her), Apprentice Support Specialist</h3>
-          <p class="paragraph p4">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Veritatis voluptatibus non, dicta harum tempora officiis laborum ipsam illum voluptatum magni libero nesciunt reprehenderit, sit dolores voluptatem esse mollitia nemo, est illo temporibus itaque!</p>
+          <p class="paragraph p4">'Impact Zone' would like to honor Ms. Raspberry for all of the kindness, enthusiasm, and dedication she continually shows for all of her students/apprentices here at Alphaworks. Thank you for being so down to earth and personally taking an interest in our success!</p>
         </div>
       </div>
   </div>  
--- template-parts/projects.php
+++ template-parts/projects.php
@@ -7,9 +7,9 @@
         <div class="card">
         <div class="content">
            <!-- image goes here -->
-           <img src="http://portfolio.local/wp-content/uploads/2022/07/Screenshot-2022-02-02-195159-e1658453735741.png" class="card-image"alt="">
+           <img src="http://portfolio.local/wp-content/uploads/2022/07/resume-logo.png" class="card-image"alt="">
            <div class="details">
-             <a href="https://kylewashington73.github.io/Donut-Shop/"><div class="link-image">card 1</div></a>
+             <a href="https://kylewashington73.github.io/Donut-Shop/"><div class="link-image">project 1</div></a>
            </div>
            <div class="media-icons">
              <a href="#"><i class="fab fa-facebook-f"></i></a>
@@ -22,9 +22,9 @@
         <div class="card">
         <div class="content">
            <!-- image goes here -->
-           <img src="http://portfolio.local/wp-content/uploads/2022/07/disney-logo.jpg" class="card-image"alt="">
+           <img src="http://portfolio.local/wp-content/uploads/2022/07/resume-logo.png" class="card-image"alt="">
            <div class="details">
-           <a href="https://chaisaetern.github.io/Replica-Challenge/"><div class="link-image">card 2</div></a>
+           <a href="https://chaisaetern.github.io/Replica-Challenge/"><div class="link-image">project 2</div></a>
            </div>
            <div class="media-icons">
              <a href="#"><i class="fab fa-facebook-f"></i></a>
@@ -38,9 +38,9 @@
         <div class="card">
         <div class="content">
            <!-- image goes here -->
-           <img src="http://portfolio.local/wp-content/uploads/2022/07/heroimg.jpg" class="card-image"alt="">
+           <img src="http://portfolio.local/wp-content/uploads/2022/07/resume-logo.png" class="card-image"alt="">
            <div class="details">
-           <a href="https://kylewashington73.github.io/children-of-incarcerated-parents/"><div class="link-image">card 3</div></a>
+           <a href="https://kylewashington73.github.io/children-of-incarcerated-parents/"><div class="link-image">project 3</div></a>
            </div>
            <div class="media-icons">
              <a href="#"><i class="fab fa-facebook-f"></i></a>
@@ -56,8 +56,8 @@
          <div class="content">
            <!-- image goes here -->
            <div class="details">
-           <img src="http://portfolio.local/wp-content/uploads/2022/06/screenshot.jpg" class="card-image"alt=""> 
-           <a href="https://kylewashington73.github.io/project-assessments/"><div class="link-image">card 4</div></a>
+           <img src="http://portfolio.local/wp-content/uploads/2022/07/resume-logo.png" class="card-image"alt=""> 
+           <a href="https://kylewashington73.github.io/project-assessments/"><div class="link-image">project 4</div></a>
            </div>
            <div class="media-icons">
              <a href="#"><i class="fab fa-facebook-f"></i></a>
@@ -71,8 +71,8 @@
          <div class="content">
            <!-- image goes here -->
            <div class="details">
-           <img src="http://portfolio.local/wp-content/uploads/2022/07/project-assessments-logo.jpg" class="card-image"alt="">
-           <a href="https://github.com/KyleWashington73/project-assessments#project-assessments"><div class="link-image">card 5</div></a>
+           <img src="http://portfolio.local/wp-content/uploads/2022/07/resume-logo.png" class="card-image"alt="">
+           <a href="https://github.com/KyleWashington73/project-assessments#project-assessments"><div class="link-image">project 5</div></a>
             
            </div>
            <div class="media-icons">
@@ -88,7 +88,7 @@
           <!-- image goes here -->
            <div class="details">
            <img src="http://portfolio.local/wp-content/uploads/2022/07/resume-logo.png" class="card-image"alt="">
-           <a href=""><div class="link-image">card 6</div></a>
+           <a href=""><div class="link-image">project 6</div></a>
            </div>
            <div class="media-icons">
              <a href="#"><i class="fab fa-facebook-f"></i></a>
