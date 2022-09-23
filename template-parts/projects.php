ea1e6417aa3b601025fb8fd37f424b99ec18c681
Grace Birnam <gbirnam@alphaworks.tech>
2022-09-22 18:36:00 -0700

    added new porject section template

--- template-parts/projects.php
+++ template-parts/projects.php
@@ -1,107 +1,46 @@
-<!-- This is the start of the slider -->
-<div class="container" id="projects">
-    <input type="radio" name="dot" id="one">
-    <input type="radio" name="dot" id="two">
-    <div class="main-card">
-      <div class="cards">
-        <div class="card">
-        <div class="content">
-           <!-- image goes here -->
-           <img src="http://impact-zone.local/wp-content/uploads/2022/09/resume-logo.png" class="card-image"alt="">
-           <div class="details">
-             <a href="https://kylewashington73.github.io/Donut-Shop/"><div class="link-image">Water Scarcity</div></a>
-           </div>
-           <div class="media-icons">
-             <a href="#"><i class="fab fa-facebook-f"></i></a>
-             <a href="#"><i class="fab fa-twitter"></i></a>
-             <a href="#"><i class="fab fa-instagram"></i></a>
-             <a href="#"><i class="fab fa-youtube"></i></a>
-           </div>
-         </div>
-        </div>
-        <div class="card">
-        <div class="content">
-           <!-- image goes here -->
-           <img src="http://impact-zone.local/wp-content/uploads/2022/09/resume-logo.png" class="card-image"alt="">
-           <div class="details">
-           <a href="https://chaisaetern.github.io/Replica-Challenge/"><div class="link-image">Karat Site</div></a>
-           </div>
-           <div class="media-icons">
-             <a href="#"><i class="fab fa-facebook-f"></i></a>
-             <a href="#"><i class="fab fa-twitter"></i></a>
-             <a href="#"><i class="fab fa-instagram"></i></a>
-             <a href="#"><i class="fab fa-youtube"></i></a>
-           </div>
-         </div>
-        </div>
-        
-        <div class="card">
-        <div class="content">
-           <!-- image goes here -->
-           <img src="http://impact-zone.local/wp-content/uploads/2022/09/resume-logo.png" class="card-image"alt="">
-           <div class="details">
-           <a href="https://kylewashington73.github.io/children-of-incarcerated-parents/"><div class="link-image">project 3</div></a>
-           </div>
-           <div class="media-icons">
-             <a href="#"><i class="fab fa-facebook-f"></i></a>
-             <a href="#"><i class="fab fa-twitter"></i></a>
-             <a href="#"><i class="fab fa-instagram"></i></a>
-             <a href="#"><i class="fab fa-youtube"></i></a>
-           </div>
-         </div>
-        </div>
-      </div>
-      <div class="cards">
-        <div class="card">
-         <div class="content">
-           <!-- image goes here -->
-           <div class="details">
-           <img src="http://impact-zone.local/wp-content/uploads/2022/09/resume-logo.png" class="card-image"alt=""> 
-           <a href="https://kylewashington73.github.io/project-assessments/"><div class="link-image">project 4</div></a>
-           </div>
-           <div class="media-icons">
-             <a href="#"><i class="fab fa-facebook-f"></i></a>
-             <a href="#"><i class="fab fa-twitter"></i></a>
-             <a href="#"><i class="fab fa-instagram"></i></a>
-             <a href="#"><i class="fab fa-youtube"></i></a>
-           </div>
-         </div>
-        </div>
-        <div class="card">
-         <div class="content">
-           <!-- image goes here -->
-           <div class="details">
-           <img src="http://impact-zone.local/wp-content/uploads/2022/09/resume-logo.png" class="card-image"alt="">
-           <a href="https://github.com/KyleWashington73/project-assessments#project-assessments"><div class="link-image">project 5</div></a>
-            
-           </div>
-           <div class="media-icons">
-             <a href="#"><i class="fab fa-facebook-f"></i></a>
-             <a href="#"><i class="fab fa-twitter"></i></a>
-             <a href="#"><i class="fab fa-instagram"></i></a>
-             <a href="#"><i class="fab fa-youtube"></i></a>
-           </div>
-         </div>
-        </div>
-        <div class="card">
-         <div class="content">
-          <!-- image goes here -->
-           <div class="details">
-           <img src="http://impact-zone.local/wp-content/uploads/2022/09/resume-logo.png" class="card-image"alt="">
-           <a href=""><div class="link-image">project 6</div></a>
-           </div>
-           <div class="media-icons">
-             <a href="#"><i class="fab fa-facebook-f"></i></a>
-             <a href="#"><i class="fab fa-twitter"></i></a>
-             <a href="#"><i class="fab fa-instagram"></i></a>
-             <a href="#"><i class="fab fa-youtube"></i></a>
-           </div>
-         </div>
-        </div>
-      </div>
-    </div>
-    <div class="button">
-      <label for="one" class=" active one"></label>
-      <label for="two" class="two"></label>
-    </div>
-  </div>
+<div class="slider">
+		<input type="radio" name="testimonial" id="t-1">
+		<input type="radio" name="testimonial" id="t-2">
+		<input type="radio" name="testimonial" id="t-3" checked>
+		<input type="radio" name="testimonial" id="t-4">
+		<input type="radio" name="testimonial" id="t-5">
+		<div class="testimonials">
+			<label class="item" for="t-1">
+				<img src="https://dummyimage.com/150" alt="picture">
+        <h1 class="site-name">Website Here</h1>
+				<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis sapiente consequatur minus ea doloremque.</p>
+				<h2>Web Developers: Names here</h2>
+			</label>
+			<label class="item" for="t-2">
+				<img src="https://dummyimage.com/150" alt="picture">
+        <h1 class="site-name">Website Here</h1>
+				<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis sapiente consequatur minus ea doloremque.</p>
+				<h2>Web Developers: Names here</h2>
+			</label>
+			<label class="item" for="t-3">
+				<img src="https://dummyimage.com/150" alt="picture">
+        <h1 class="site-name">Website Here</h1>
+				<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis sapiente consequatur minus ea doloremque.</p>
+				<h2>Web Developers: Names here</h2>
+			</label>
+			<label class="item" for="t-4">
+				<img src="https://dummyimage.com/150" alt="picture">
+        <h1 class="site-name">Website Here</h1>
+				<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis sapiente consequatur minus ea doloremque.</p>
+				<h2>Web Developers: Names here</h2>
+			</label>
+			<label class="item" for="t-5">
+				<img src="https://dummyimage.com/150" alt="picture">
+        <h1 class="site-name">Website Here</h1>
+				<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis sapiente consequatur minus ea doloremque.</p>
+				<h2>Web Developers: Names here</h2>
+			</label>
+		</div>
+		<div class="dots">
+			<label for="t-1"></label>
+			<label for="t-2"></label>
+			<label for="t-3"></label>
+			<label for="t-4"></label>
+			<label for="t-5"></label>
+		</div>
+	</div>
